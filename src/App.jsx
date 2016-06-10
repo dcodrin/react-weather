506ae1ce04621f47d6edbed27887949da0d5b36a
--replace-all <diaconu.codrin@gmail.com>
2016-06-10 14:34:10 -0400

    Modified App.jsx.

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
- static render() {
-    return (
-        <div>
-            <h1>Hello Mars! We are live!</h1>
-            {/*Application body*/}
-        </div>
-    );
-  }
+    static render() {
+        return (
+            <div>
+                <h1>Hello Mars! We are live!</h1>
+                {/*Application body*/}
+            </div>
+        );
+    }
 }
 
 App.propTypes = {};
