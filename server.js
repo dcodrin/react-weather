24b60189adca5624e2eff7b7ef7ca0d89e08a5d4
--replace-all <diaconu.codrin@gmail.com>
2016-06-10 19:34:36 -0400

    Changed index.html

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ var PORT = process.env.PORT || 3000;
 
 //redirect to http from https
 app.use(function (req, res, next) {
-    if(req.headers['x-forwarded-proto'] === 'http'){
-        next();
-    } else {
+    if(req.headers['x-forwarded-proto'] === 'https'){
         res.redirect('http://' + req.hostname + req.url);
+    } else {
+        next();
     }
 });
 
