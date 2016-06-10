3d6c0001d41955f38681881bc6c46f6c95bbfa3a
--replace-all <diaconu.codrin@gmail.com>
2016-06-10 19:23:41 -0400

    Generated bundle.js.

--- server.js
+++ server.js
@@ -2,6 +2,15 @@ var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 3000;
 
+//redirect to http from https
+app.use(function (req, res, next) {
+    if(req.headers['x-forwared-proto'] === 'http'){
+        next();
+    } else {
+        res.redirect('http://' + req.hostname + req.url);
+    }
+});
+
 app.use(express.static('public'));
 
 app.listen(PORT, function () {
