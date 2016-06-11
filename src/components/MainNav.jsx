13aae82661c85f38bbe7cc56786a9a0bfe13dd09
--replace-all <diaconu.codrin@gmail.com>
2016-06-11 13:10:50 -0400

    Modified MainNav

--- src/components/MainNav.jsx
+++ src/components/MainNav.jsx
@@ -1,13 +1,47 @@
 import React from 'react';
 import {Link, IndexLink} from 'react-router';
 
-export default () => {
-    return (
-        <div>
-            <h1>Here Be Thy Navigation</h1>
-            <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Get Weather</IndexLink>
-            <Link to="about" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>The About</Link>
-            <Link to="examples" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Examples</Link>
-        </div>
-    );
-};
+class MainNav extends React.Component {
+
+    constructor(){
+        super();
+
+        this.onSearch = this.onSearch.bind(this);
+    }
+
+    onSearch(e){
+        e.preventDefault();
+        alert('Not yet wired up!');
+    }
+
+    render() {
+        return (
+            <div className="top-bar">
+                <div className="top-bar-left">
+                    <ul className="menu">
+                        <li className="menu-text">React Weather</li>
+                        <li><IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Get
+                            Weather</IndexLink></li>
+                        <li><Link to="about" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>The
+                            About</Link></li>
+                        <li><Link to="examples" activeClassName="active"
+                                  activeStyle={{fontWeight: 'bold'}}>Examples</Link></li>
+                    </ul>
+                </div>
+                <div className="tob-bar-right">
+                    <form onSubmit={this.onSearch}>
+                        <ul className="menu">
+                            <li><input type="search" placeholder="City name"/></li>
+                            <li><input type="submit" className="button" value="Get Weather"/></li>
+                        </ul>
+                    </form>
+                </div>
+            </div>
+        );
+    }
+}
+
+MainNav.propTypes = {};
+MainNav.defaultProps = {};
+
+export default MainNav;
